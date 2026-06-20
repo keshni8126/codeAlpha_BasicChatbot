190ca3d5564136427c4da058b0b90daf77cc8a21
keshni8126 <keshni8126@gmail.com>
2026-06-20 19:09:24 +0530

    Add files via upload

---
+++ CodeAlpha_BasicChatbot.py
@@ -0,0 +1,17 @@
+print("Welcome to my chatbot!:")
+print("Type 'okay tata' to exit.")
+while True :
+    radha = input("you:")
+    if radha=="hello":
+        print("Bot:Hi!")
+    elif radha =="how are you":
+        print("Bot: Im fine!")
+    elif radha =="are you interested to talk to me":
+        print("Bot:yes,of course!")
+    elif radha=="thank you":
+        print("Bot: You're welcome!")
+    elif radha =="okay tata":
+        print("Bot:yeah,tata friend!")
+        break
+    
+    
